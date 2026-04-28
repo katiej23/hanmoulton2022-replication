5d929bd1cb83b1553a9335889cb1fe8f7449ab28
Katherine Johnson <katiej23@stanford.edu>
2026-04-27 23:13:52 -0700

    upload files for moving window experiment

---
+++ stims.js
@@ -0,0 +1,20 @@
+// stims.js for Self-Paced Reading Tutorial
+
+const experimentStimuli = [
+  {
+    item: 1,
+    condition: "upper_bound",
+    sentence: "Every lawyer made their case successfully.",
+    target_word: "rest",
+    question: "This is a comprehension question!",
+    correct_answer: "No"
+  },
+  {
+    item: 1,
+    condition: "upper_bound",
+    sentence: "A lawyer made their case successfully.",
+    target_word: "rest",
+    question: "This is a comprehension question!",
+    correct_answer: "No"
+  }
+];
