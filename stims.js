0da44cc5be37326bde51f757e5b199f086e9667a
Katherine Johnson <katiej23@stanford.edu>
2026-04-27 23:57:29 -0700

    new stimuli

--- stims.js
+++ stims.js
@@ -3,17 +3,145 @@
 const experimentStimuli = [
   {
     item: 1,
-    condition: "upper_bound",
-    sentence: "Every lawyer made their case successfully.",
-    target_word: "rest",
+    condition: "quantificational_they",
+    sentence: "Just one grandchild could inherit the jewelry. Every grandchild hoped that the one who would inherit the jewelry would be them when the will was finally announced.",
+    // target_word: "rest",
     question: "This is a comprehension question!",
     correct_answer: "No"
   },
   {
     item: 1,
-    condition: "upper_bound",
-    sentence: "A lawyer made their case successfully.",
-    target_word: "rest",
+    condition: "generic_they",
+    sentence: "Just one grandchild could inherit the jewelry. TBD stimuli for this sentence.",
+    // target_word: "rest",
+    question: "This is a comprehension question!",
+    correct_answer: "No"
+  },
+  {
+    item: 1,
+    condition: "specific_they",
+    sentence: "Just one grandchild could inherit the jewelry. The youngest grandchild hoped that the one who would inherit the jewelry would be them when the will was finally announced.",
+    // target_word: "rest",
+    question: "This is a comprehension question!",
+    correct_answer: "No"
+  },
+  {
+    item: 1,
+    condition: "named_they",
+    sentence: "Just one grandchild could inherit the jewelry. Alex hoped that the one who would inherit the jewelry would be them when the will was finally announced.",
+    // target_word: "rest",
+    question: "This is a comprehension question!",
+    correct_answer: "No"
+  },
+  {
+    item: 1,
+    condition: "quantificational_him",
+    sentence: "Just one grandchild could inherit the jewelry. Every grandchild hoped that the one who would inherit the jewelry would be him when the will was finally announced.",
+    // target_word: "rest",
+    question: "This is a comprehension question!",
+    correct_answer: "No"
+  },
+  {
+    item: 1,
+    condition: "generic_him",
+    sentence: "Just one grandchild could inherit the jewelry. TBD stimuli for this sentence.",
+    // target_word: "rest",
+    question: "This is a comprehension question!",
+    correct_answer: "No"
+  },
+  {
+    item: 1,
+    condition: "specific_him",
+    sentence: "Just one grandchild could inherit the jewelry. The youngest grandchild hoped that the one who would inherit the jewelry would be him when the will was finally announced.",
+    // target_word: "rest",
+    question: "This is a comprehension question!",
+    correct_answer: "No"
+  },
+  {
+    item: 1,
+    condition: "named_him",
+    sentence: "Just one grandchild could inherit the jewelry. Alex hoped that the one who would inherit the jewelry would be him when the will was finally announced.",
+    // target_word: "rest",
+    question: "This is a comprehension question!",
+    correct_answer: "No"
+  },
+  {
+    item: 1,
+    condition: "quantificational_they",
+    sentence: "Only one competitor could win the race. Every competitor thought that the one who would win the race would be them when the times were finally announced.",
+    // target_word: "rest",
+    question: "This is a comprehension question!",
+    correct_answer: "No"
+  },
+  {
+    item: 1,
+    condition: "generic_they",
+    sentence: "Only one competitor could win the race. TBD stimuli for this sentence.",
+    // target_word: "rest",
+    question: "This is a comprehension question!",
+    correct_answer: "No"
+  },
+  {
+    item: 1,
+    condition: "specific_they",
+    sentence: "Only one competitor could win the race. The youngest competitor thought that the one who would win the race would be them when the times were finally announced.",
+    // target_word: "rest",
+    question: "This is a comprehension question!",
+    correct_answer: "No"
+  },
+  {
+    item: 1,
+    condition: "named_they",
+    sentence: "Only one competitor could win the race. Morgan that the one who would win the race would be them when the times were finally announced.",
+    // target_word: "rest",
+    question: "This is a comprehension question!",
+    correct_answer: "No"
+  },
+  {
+    item: 1,
+    condition: "quantificational_him",
+    sentence: "Only one competitor could win the race. Every competitor thought that the one who would win the race would be him when the times were finally announced.",
+    // target_word: "rest",
+    question: "This is a comprehension question!",
+    correct_answer: "No"
+  },
+  {
+    item: 1,
+    condition: "generic_him",
+    sentence: "Only one competitor could win the race. TBD stimuli for this sentence.",
+    // target_word: "rest",
+    question: "This is a comprehension question!",
+    correct_answer: "No"
+  },
+  {
+    item: 1,
+    condition: "specific_him",
+    sentence: "Only one competitor could win the race. The youngest competitor thought that the one who would win the race would be him when the times were finally announced.",
+    // target_word: "rest",
+    question: "This is a comprehension question!",
+    correct_answer: "No"
+  },
+  {
+    item: 1,
+    condition: "named_him",
+    sentence: "Only one competitor could win the race. Morgan that the one who would win the race would be him when the times were finally announced.",
+    // target_word: "rest",
+    question: "This is a comprehension question!",
+    correct_answer: "No"
+  },
+  {
+    item: 1,
+    condition: "filler",
+    sentence: "Ben was building a train set alone in the garage. After finishing the model train set from the hobby shop's Christmas sale, he went to the store to buy another one.",
+    // target_word: "rest",
+    question: "This is a comprehension question!",
+    correct_answer: "No"
+  },
+  {
+    item: 1,
+    condition: "filler",
+    sentence: "John was studying alone in the bookstore. After reading the math and science textbooks from the school library, he dreamed about proving an important theorem.",
+    // target_word: "rest",
     question: "This is a comprehension question!",
     correct_answer: "No"
   }
